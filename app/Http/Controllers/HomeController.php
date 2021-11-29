4b5df13ddf73023801cfadd0e4ee91e6861fa765
koheihida <88849254+koheihida@users.noreply.github.com>
2021-11-29 20:29:10 +0900

    ViewComposerを使用して$memosのリファクタリング

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,13 +27,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $memos = Memo::select('memos.*')
-            ->where('user_id', '=', \Auth::id())
-            ->whereNull('deleted_at')
-            ->orderBy('updated_at','DESC')
-            ->get();
+        // $memos = Memo::select('memos.*')
+        //     ->where('user_id', '=', \Auth::id())
+        //     ->whereNull('deleted_at')
+        //     ->orderBy('updated_at','DESC')
+        //     ->get();
         $tags =Tag::where('user_id','=',\Auth::id())->whereNull('deleted_at')->orderBy('id','DESC')->get();
-        return view('create',compact('memos','tags'));
+        return view('create',compact('tags'));
     }
 
     public function store(Request $request)
@@ -58,11 +58,11 @@ class HomeController extends Controller
 
     public function edit($id)
     {
-        $memos = Memo::select('memos.*')
-            ->where('user_id', '=', \Auth::id())
-            ->whereNull('deleted_at')
-            ->orderBy('updated_at','DESC')
-            ->get();
+        // $memos = Memo::select('memos.*')
+        //     ->where('user_id', '=', \Auth::id())
+        //     ->whereNull('deleted_at')
+        //     ->orderBy('updated_at','DESC')
+        //     ->get();
         
         $edit_memo = Memo::select('memos.*','tags.id AS tag_id')
             ->leftJoin('memo_tags','memo_tags.memo_id','=','memos.id')
@@ -78,7 +78,7 @@ class HomeController extends Controller
         }
         $tags =Tag::where('user_id','=',\Auth::id())->whereNull('deleted_at')->orderBy('id','DESC')->get();
         
-        return view('edit', compact('memos','edit_memo', 'include_tags', 'tags'));
+        return view('edit', compact('edit_memo', 'include_tags', 'tags'));
     }
 
     public function update(Request $request)
