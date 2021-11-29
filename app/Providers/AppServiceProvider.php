c1186a17ffa302b27c2bd92359fa6abaabdfa276
koheihida <88849254+koheihida@users.noreply.github.com>
2021-11-29 21:12:24 +0900

    タグからの絞り込み検索機能の実装

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,11 +26,22 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->composer('*',function($view){
-            $memos = Memo::select('memos.*')
-                ->where('user_id','=', \Auth::id())
-                ->whereNull('deleted_at')
-                ->orderBy('updated_at','DESC')
-                ->get();
+            $query_tag = \Request::query('tag');
+            if (!empty($query_tag)){
+                $memos = Memo::select('memos.*')
+                    ->leftJoin('memo_tags','memo_tags.memo_id','=','memos.id')
+                    ->where('memo_tags.tag_id','=',$query_tag)
+                    ->where('user_id','=', \Auth::id())
+                    ->whereNull('deleted_at')
+                    ->orderBy('updated_at','DESC')
+                    ->get();
+            }else{
+                $memos = Memo::select('memos.*')
+                    ->where('user_id','=', \Auth::id())
+                    ->whereNull('deleted_at')
+                    ->orderBy('updated_at','DESC')
+                    ->get();
+            }
             $tags = Tag::where('user_id','=', \Auth::id())
                 ->whereNull('deleted_at')
                 ->orderBy('id', 'DESC')
